783127fb06553fbab40481a0743be63eec5964ca
MehakCoder <cmehak26@gmail.com>
2022-05-24 11:29:03 +0530

    add shop and index

--- js/index.js
+++ js/index.js
@@ -10,3 +10,16 @@ $(".product-filters li").on('click', function () {
   });
   
 });
+$(".product-filters .strawberry").on('click', function () {
+            
+  $(".product-filters li").removeClass("active");
+  $(this).addClass("active");
+
+  var selector = $(this).attr('data-filter');
+
+  $(".product-lists").isotope({
+      filter: Strawberry,
+  });
+  
+});
+
