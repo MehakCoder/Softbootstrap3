1f1eaf465ae844593c28be95e9fa86aea964a077
MehakCoder <cmehak26@gmail.com>
2022-05-26 16:51:35 +0530

    index

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:3
-      },
-      1000:{
-          items:5
-      }
-  }
+$(document).ready(function(){
+  $('.homepage-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+  })  
 })
 
 $(".product-filters li").on('click', function () {
