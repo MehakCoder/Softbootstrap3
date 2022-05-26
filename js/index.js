c999a3376bae0f1dc65cf689a87abfc84631ddb3
MehakCoder <cmehak26@gmail.com>
2022-05-26 16:39:05 +0530

    add index

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+
 $(".product-filters li").on('click', function () {
             
   $(".product-filters li").removeClass("active");
